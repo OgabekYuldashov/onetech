5e91331b13766ec2b92792a8e9e06467527feef3
henok keraga <henmkeraga1@gmail.com>
2019-08-11 16:30:23 -0500

    product

--- src/main/java/com/mum/onetech/controller/ProductController.java
+++ src/main/java/com/mum/onetech/controller/ProductController.java
@@ -1,44 +1,46 @@
 package com.mum.onetech.controller;
 
-import com.mum.onetech.domain.Category;
 import com.mum.onetech.domain.Product;
-import com.mum.onetech.service.CategoryService;
-import com.mum.onetech.service.ProductService;
-import com.mum.onetech.util.Util;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
+@RequestMapping("/product")
 @Controller
 public class ProductController {
-    @Autowired
-    CategoryService categoryService;
-    @Autowired
-    ProductService productService;
 
-    @ModelAttribute("categories")
-    List<Category> getAllCategories(){
-        return categoryService.findAll();
+    @GetMapping("/")
+    public String getProductForm(@ModelAttribute("product") Product product){
+
+        return "product";
     }
 
-    @GetMapping("/products")
-    public String getAllProducts(@RequestParam(name = "cat", required = false) String catId, Model model){
-        if(Util.isPositiveInteger(catId)){
-            model.addAttribute("products", productService.findAllByCategoryId(Long.valueOf(catId)));
-            model.addAttribute("count", productService.getCountByCategoryId(Long.valueOf(catId)));
-        }else {
-            model.addAttribute("products", productService.findAll());
-            model.addAttribute("count", productService.getCountAll());
-        }
 
-        Product p = new Product();
 
-        return "shop";
-    }
+//    @Autowired
+//    CategoryService categoryService;
+//    @Autowired
+//    ProductService productService;
+//
+//    @ModelAttribute("categories")
+//    List<Category> getAllCategories(){
+//        return categoryService.findAll();
+//    }
+//
+//    @GetMapping("/products")
+//    public String getAllProducts(@RequestParam(name = "cat", required = false) String catId, Model model){
+//        if(Util.isPositiveInteger(catId)){
+//            model.addAttribute("products", productService.findAllByCategoryId(Long.valueOf(catId)));
+//            model.addAttribute("count", productService.getCountByCategoryId(Long.valueOf(catId)));
+//        }else {
+//            model.addAttribute("products", productService.findAll());
+//            model.addAttribute("count", productService.getCountAll());
+//        }
+//
+//        Product p = new Product();
+//
+//        return "shop";
+//    }
 
 }
--- src/main/java/com/mum/onetech/controller/RegistrationController.java
+++ src/main/java/com/mum/onetech/controller/RegistrationController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -23,6 +20,10 @@ public class RegistrationController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+    @PostMapping("/login")
+    public String welcome(){
+        return "welcome";
     }
 
 
