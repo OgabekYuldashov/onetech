464bdd407a5dd53a130cfb3b2cc5575f4d22bd91
zelalemzergaw <50334133+zelalemzergaw@users.noreply.github.com>
2019-08-13 21:55:47 -0500

    product sql

--- src/main/java/com/mum/onetech/controller/AdminController.java
+++ src/main/java/com/mum/onetech/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.mum.onetech.controller;
 
+import com.mum.onetech.domain.Credentials;
 import com.mum.onetech.domain.Seller;
 import com.mum.onetech.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class AdminController {
 
     /*//////////////////////////////////*/
 
-    @GetMapping("/sellers")
+    @GetMapping("/seller")
     public String listSellersInformation(@RequestParam(value = "status", required = false) String status, Seller seller, Model model) {
 
         List<Seller> sellersToReturn = new ArrayList<>();
@@ -40,8 +41,51 @@ public class AdminController {
                 sellersToReturn = sellerService.getUnverifiedSellers();
             }
         }
+        //.....
+        Credentials credentials1 =new Credentials();
+        Credentials credentials2 =new Credentials();
+        Credentials credentials3 =new Credentials();
+        Credentials credentials4=new Credentials();
+        credentials1.setFirstName("Zelalem");
+        credentials2.setFirstName("Henok");
+        credentials3.setFirstName("Oga'bek");
+        credentials4.setFirstName("Abel");
+        credentials1.setLastName("Zergaw");
+        credentials2.setLastName("Melese");
+        credentials3.setLastName("Yuldov");
+        credentials4.setLastName("Tesfaye");
+        credentials1.setEmail("zz@gmail.com");
+        credentials2.setEmail("zz@yahoo.com");
+        credentials3.setEmail("zz@hotmail.com");
+        credentials4.setEmail("zz@hotoklikjmail.com");
+        credentials1.setVerified(1);
+        credentials2.setVerified(0);
+        credentials3.setVerified(0);
+        credentials4.setVerified(1);
+        Seller seller1= new Seller();
+        Seller seller2 = new Seller();
+        Seller seller3= new Seller();
+        Seller seller4 = new Seller();
+        seller1.setCredentials(credentials1);
+        seller2.setCredentials(credentials2);
+        seller3.setCredentials(credentials3);
+        seller4.setCredentials(credentials4);
 
-        model.addAttribute("sellers", sellersToReturn);
+        // credentials2.setVerified(true);
+        seller1.setId(3l);
+        seller2.setId(2l);
+        seller4.setId(4l);
+        seller3.setId(1l);
+        sellersToReturn.add(seller1);
+        sellersToReturn.add(seller2);
+        sellersToReturn.add(seller3);
+        sellersToReturn.add(seller4);
+
+        //....
+
+
+
+        model.addAttribute("sellerList", sellersToReturn);
         return "sellers";
     }
 
