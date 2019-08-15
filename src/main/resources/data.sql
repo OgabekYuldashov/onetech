9f1df4e8a6a4e451559a6aea47d7d2a526d4df31
oyuldashov-mum <oyuldashov@mum.edu>
2019-08-15 09:32:12 -0500

    Spring Security User Authentication Information Done

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+create table if not exists persistent_logins (
+  username varchar_ignorecase(100) not null,
+  series varchar(64) primary key,
+  token varchar(64) not null,
+  last_used timestamp not null
+  );
 
 INSERT INTO Category (`NAME`) VALUES ('Computers & Laptops');
 INSERT INTO Category (`NAME`) VALUES ('Cameras & Photos');
